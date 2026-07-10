d9b4055a6c9f2f7d7c1ae9fe79569277df35703d
ma1gnan <119422502+ma1gnan@users.noreply.github.com>
2026-07-10 19:51:22 -0400

    Refactor EIA API request parameters and URL
    Updated API parameters and fixed URL encoding.

--- eia_test.py
+++ eia_test.py
@@ -1,27 +1,31 @@
 # eia test quick script
 
 import json
+import ssl
 import urllib.parse
 import urllib.request
 
 # setting up the parameters from eia website
 API_KEY = v2BpqEvCotQIRlCgiAlmfTowZcEJl1JIaPTwooPj
-BASE_URL = https://api.eia.gov/v2/electricity/rto/region-data/data/?frequency=hourly&data[0]=value&start=2019-01-01T00&end=2026-07-10T00&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000
+BASE_URL = "https://api.eia.gov/v2/electricity/rto/region-data/data/"
 
-X-Params: {
+params= {
+    "api_key": API_KEY,
     "frequency": "hourly",
-    "data": [
-        "value"
-    ],
-    "facets": {},
-    "start": "2019-01-01T00",
-    "end": "2026-07-10T00",
-    "sort": [
-        {
-            "column": "period",
-            "direction": "desc"
-        }
-    ],
-    "offset": 0,
-    "length": 5000
+    "data[0]": "value",
+    "facets[respondent][]": "SOCO",
+    "start": "2026-05-01T00",
+    "end": "2026-05-31T23",
+    "sort[0][column]": "period",
+    "sprt[0][direction]": "asc",
 }
+
+# Encode parameters and request data
+url_mod = f"{BASE_URL}?{urllib.parse.urlencode(params)}"
+
+try:
+    with urllib.request.urlopen(url_mod) as response:
+        raw_data = json.loads("response.read().decode())
+
+        #print verification information
+        records = raw_data["response"]["data"]
